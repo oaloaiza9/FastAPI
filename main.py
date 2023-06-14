ec836122ee67a0c3a7366b61e48de5533275455a
oaloaiza9 <oaloaiza9@misena.edu.co>
2023-06-14 09:11:14 -0500

    Primer commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Datos de ejemplo de clientes
+clientes = [
+    {
+        "documento": "12345678",
+        "nombres": "Juan",
+        "apellidos": "Pérez"
+    },
+    {
+        "documento": "98765432",
+        "nombres": "María",
+        "apellidos": "Gómez"
+    }
+]
+
+@app.get("/clientes")
+async def get_clientes():
+    return clientes
