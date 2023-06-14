6f0e0373759d977f4c25f72068c9a0929a1198bf
oaloaiza9 <oaloaiza9@misena.edu.co>
2023-06-14 09:55:37 -0500

    Agregando endpoints

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-# Datos de ejemplo de clientes
-clientes = [
-                {
-                    "documento": "12345678",
-                    "nombres": "Juan",
-                    "apellidos": "Pérez"
-                },
-                {
-                    "documento": "98765432",
-                    "nombres": "María",
-                    "apellidos": "Gómez"
-                }
-            ]
+class Cliente(BaseModel):
+    documento: str
+    nombres: str
+    apellidos: str
+
+clientes = []
 
 @app.get("/clientes")
 async def get_clientes():
     return clientes
+
+@app.post("/clientes")
+def agregar_cliente(cliente: Cliente):
+    clientes.append(cliente)
+    return {"mensaje": "Cliente agregado correctamente"}
+
+@app.put("/clientes/{documento}")
+def editar_cliente(documento: str, cliente: Cliente):
+    for i, c in enumerate(clientes):
+        if c.documento == documento:
+            clientes[i] = cliente
+            return {"mensaje": "Cliente editado correctamente"}
+    return {"error": "Cliente no encontrado"}
+
+@app.delete("/clientes/{documento}")
+def eliminar_cliente(documento: str):
+    for i, c in enumerate(clientes):
+        if c.documento == documento:
+            clientes.pop(i)
+            return {"mensaje": "Cliente eliminado correctamente"}
+    return {"error": "Cliente no encontrado"}
