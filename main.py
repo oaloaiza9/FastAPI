e3bc4c4676b8e2885ef3f55c92dc224db724db19
oaloaiza9 <oaloaiza9@misena.edu.co>
2023-06-14 09:49:23 -0500

    Modificando lista

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ app = FastAPI()
 
 # Datos de ejemplo de clientes
 clientes = [
-    {
-        "documento": "12345678",
-        "nombres": "Juan",
-        "apellidos": "Pérez"
-    },
-    {
-        "documento": "98765432",
-        "nombres": "María",
-        "apellidos": "Gómez"
-    }
-]
+                {
+                    "documento": "12345678",
+                    "nombres": "Juan",
+                    "apellidos": "Pérez"
+                },
+                {
+                    "documento": "98765432",
+                    "nombres": "María",
+                    "apellidos": "Gómez"
+                }
+            ]
 
 @app.get("/clientes")
 async def get_clientes():
