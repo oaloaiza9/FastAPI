f30ec11ccb335f840771a4f10b9b56bb9c39684c
oaloaiza9 <oaloaiza9@misena.edu.co>
2023-06-14 10:27:45 -0500

    Agregando lista usuarios

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import hashlib
+
+def encrypt_md5(text):
+    md5_hash = hashlib.md5(text.encode('utf-8'))
+    encrypted_text = md5_hash.hexdigest()
+    return encrypted_text
+
 
 app = FastAPI()
+
+class Usuarios(BaseModel):
+    email: str
+    password: str
 
 class Cliente(BaseModel):
     documento: str
     nombres: str
     apellidos: str
 
+usuarios = [
+    Usuarios(email="andres@mail.com", password=encrypt_md5("12345"))
+]
+
 clientes = [
     Cliente(documento="12345678", nombres="John", apellidos="Doe"),
     Cliente(documento="87654321", nombres="Jane", apellidos="Smith"),
     Cliente(documento="12345", nombres="Juan", apellidos="Perez")
 ]
+
+@app.get("/usuarios")
+async def get_usuarios():
+    return usuarios
 
 @app.get("/clientes")
 async def get_clientes():
